66ff318cdb4a0d0b9fb4aa366392ec222be863c2
YuHung Lo <33674612+lorekkusu@users.noreply.github.com>
2023-04-04 05:13:54 +0000

    Optimize install.sh & uninstall.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# backup old files
-for DIR in $(ls -A .dotfiles); do
-    for FILE in $(ls -A .dotfiles/$DIR); do
-        if [ -f ~/$FILE ]; then
-            mv ~/$FILE ~/$FILE.bak
-        fi
+directory_backup=".dotfiles.bak"
+directory_dotfiles=".dotfiles"
 
-        # create symlinks
-        ln -sv "$(pwd)/.dotfiles/$DIR/$FILE" ~/$FILE
-    done
+# if backup directory exists: stop
+if [ -d $HOME/$directory_backup ]; then
+    echo "Backup directory already exists. Aborting."
+    exit 1
+fi
+
+mkdir -p $HOME/$directory_backup
+for dotfile_source in $(find "$(pwd)/$directory_dotfiles" -type f); do
+    filename=$(basename $dotfile_source)
+    dotfile_target="$HOME/$filename"
+    dotfile_backup="$HOME/$directory_backup/$filename"
+
+    # backup old files
+    if [ -f $dotfile_target ]; then
+        mv $dotfile_target $dotfile_backup
+    else
+        touch $dotfile_backup
+    fi
+
+    # create symlinks
+    ln -sv $dotfile_source $dotfile_target
 done
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-# remove symlinks
-for DIR in $(ls -A .dotfiles); do
-    for FILE in $(ls -A .dotfiles/$DIR); do
-        if [ -L ~/$FILE ]; then
-            rm ~/$FILE
-        fi
+directory_backup=".dotfiles.bak"
 
-        # restore old files
-        if [ -f ~/$FILE.bak ]; then
-            mv ~/$FILE.bak ~/$FILE
-        fi
-    done
+# if backup directory doesn't exist: stop
+if [ ! -d $HOME/$directory_backup ]; then
+    echo "Backup directory doesn't exist. Aborting."
+    exit 1
+fi
+
+# restore files
+for dotfile_backup in $(find $HOME/$directory_backup -type f); do
+    filename=$(basename $dotfile_backup)
+    dotfile_target="$HOME/$filename"
+
+    # delete target files
+    if [ -f $dotfile_target ]; then
+        rm $dotfile_target
+    fi
+
+    # restore backup files
+    mv -v $dotfile_backup $dotfile_target
 done
+rm -rf $HOME/$directory_backup
