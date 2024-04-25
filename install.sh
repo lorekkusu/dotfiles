aba909e4871585b72bc8219a5582da6e2a096837
YuHung Lo <33674612+lorekkusu@users.noreply.github.com>
2024-04-25 13:13:15 +0800

    Update iterm

--- install.sh
+++ install.sh
@@ -17,7 +17,3 @@ fi
 # Link dotfiles (any)
 echo "Linking dotfiles (any)"
 ln -svf $PWD/any/.gitconfig $HOME/.gitconfig
-
-# Link preferences
-mkdir -p $HOME/.iterm
-ln -svf $PWD/iterm/com.googlecode.iterm2.plist $HOME/.iterm/com.googlecode.iterm2.plist
