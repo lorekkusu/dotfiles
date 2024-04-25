bd0d031fe3f321c6e4aa82a1c194a97ec132ea61
YuHung Lo <33674612+lorekkusu@users.noreply.github.com>
2024-04-25 11:37:03 +0800

    Add item2 plist

--- install.sh
+++ install.sh
@@ -17,3 +17,7 @@ fi
 # Link dotfiles (any)
 echo "Linking dotfiles (any)"
 ln -svf $PWD/any/.gitconfig $HOME/.gitconfig
+
+# Link preferences
+mkdir -p $HOME/.iterm
+ln -svf $PWD/iterm/com.googlecode.iterm2.plist $HOME/.iterm/com.googlecode.iterm2.plist
