c9cce3f2cb4d8ae0a1d90c5cc51273b544366894
YuHung Lo <33674612+lorekkusu@users.noreply.github.com>
2024-04-28 17:57:19 +0800

    feat: Rearrange

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# Install TPM if not installed
+if ! command -v tmux &> /dev/null
+then
+  echo "Installing TPM"
+  git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+fi
+
+# Install Oh My Zsh if not installed
+if ! command -v omz &> /dev/null
+then
+    echo "Installing Oh My Zsh"
+    export CHSH=no
+    export RUNZSH=no
+    export KEEP_ZSHRC=yes
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+fi
+
+# Install the specific script based on platform
 PLATFORM=$(uname)
 echo "Detected platform: $PLATFORM"
-
 if [[ $PLATFORM == 'Darwin' ]]; then
     echo "macOS detected"
     source macos.sh
@@ -17,8 +37,12 @@ fi
 # Link dotfiles (any)
 echo "Linking dotfiles (any)"
 ln -svf $PWD/any/.gitconfig $HOME/.gitconfig
+ln -svf $PWD/any/.tmux.conf $HOME/.tmux.conf
+ln -svf $PWD/any/.zshenv $HOME/.zshenv
+ln -svf $PWD/any/.zprofile $HOME/.zprofile
+ln -svf $PWD/any/.zshrc $HOME/.zshrc
 
 # Link others
 echo "Linking others"
-ln -svf $PWD/iterm $HOME/.iterm
+ln -svf $PWD/iterm $HOME/.config/iterm
 ln -svf $PWD/nvim $HOME/.config/nvim
--- macos.sh
+++ macos.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Install Oh My Zsh if not installed
-if ! command -v omz &> /dev/null
-then
-    echo "Installing Oh My Zsh"
-    export CHSH=no
-    export RUNZSH=no
-    export KEEP_ZSHRC=yes
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-fi
-
 # Install Homebrew if not installed
 if ! command -v brew &> /dev/null
 then
@@ -24,9 +11,3 @@ fi
 # Install Homebrew bundle
 echo "Installing Homebrew bundle"
 brew bundle install --file=macos/Brewfile
-
-# Link dotfiles
-echo "Linking dotfiles (macos)"
-ln -svf $PWD/macos/.zshenv $HOME/.zshenv
-ln -svf $PWD/macos/.zprofile $HOME/.zprofile
-ln -svf $PWD/macos/.zshrc $HOME/.zshrc
